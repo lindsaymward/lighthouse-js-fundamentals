b795676bdbf8236a625561a33f910cd0d2962322
Lindsay Ward <lward23@hotmail.com>
2023-03-17 23:56:59 +0000

    Create function to calculate age and return string

---
+++ age-calculator.js
@@ -0,0 +1,16 @@
+/*
+Three parameters:
+name (string)
+yearOfBirth (number)
+currentYear (number)
+
+return name + " is " + age + " years old."
+*/
+
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+};
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
