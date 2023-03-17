45180f8a68ea57f4045df3e79fed5f375012413d
Lindsay Ward <lward23@hotmail.com>
2023-03-17 02:19:26 +0000

    Created a function to test whether a number is odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function (number) {
+  return !(number % 2 === 0);
+};
+
+console.log(isOdd(3));
+console.log(isOdd(6));
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
