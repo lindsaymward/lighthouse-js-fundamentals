66a2fb0338d92f29c76bbcf8272d65846d699ca7
Lindsay Ward <lward23@hotmail.com>
2023-03-21 20:28:32 +0000

    Refactor code into a function that can take in arrays as arguments for more custom use

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,10 +1,12 @@
-for (let num = 100; num <= 200; num++) {
-  let message = "";
-  if (num % 3 === 0) {
-    message = "Loopy";
+function loopyLighthouse(range, multiples, words) {
+  for (let num = range[0]; num <= range[1]; num++) {
+    let message = "";
+    if (num % multiples[0] === 0) {
+      message = words[0];
+    }
+    if (num % multiples[1] === 0) {
+      message += words[1];
+    }
+    console.log(message || num);
   }
-  if (num % 4 === 0) {
-    message += "Lighthouse";
-  }
-  console.log(message || num);
 }
